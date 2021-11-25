91de2cac24576c9d3b170095e36eb50dbb995679
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-25 14:12:32 +0100

    setup iniziale

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+      <img src="../assets/logo.png" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/style/variables';
+    header{
+        background-color: $secondary-color;
+        height: 100px;
+        padding: 10px;
+        img{
+            height: 100%;
+        }
+    }
+</style>
