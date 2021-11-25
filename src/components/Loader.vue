1ff2ac606c458f1b339afb4998b3c07cd166b8cd
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-25 22:08:02 +0100

    add bonus loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="loader text-center text-light">
+        <h1>{{ title }}</h1>
+        <i class="load-icon fas fa-circle-notch my-5"></i>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Loader',
+    props: {
+        title: String,
+    },
+}
+</script>
+
+<style scoped lang="scss">
+    h1{
+        font-size: 50px;
+    }
+    .load-icon{
+        font-size: 80px;
+        animation: rotation 1.2s linear infinite;
+        
+        @keyframes rotation {
+            0%{
+                transform: rotate(-360deg);
+            }
+        }
+    }
+</style>
--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="my-container">
-      <div class="container-fluid">
-        <ul class="d-flex flex-wrap list-unstyled justify-content-center">
+      <div class="container-fluid d-flex justify-content-center align-items-center h-100">
+        <ul v-if="MainCardsList !== null" class="d-flex flex-wrap list-unstyled justify-content-center">
             <li v-for="(card, index) in MainCardsList" :key="`${index}`" class="col-lg-2 col-md-4 col-sm-6  col-12 px-4 mb-4">
 
                 <CardProp :image="card.poster" :title="card.title" :author="card.author" :year="card.year" :genre="card.genre"/>
 
             </li>
         </ul>
-
+        <Loader v-else title="ALBUM LIST"/>
       </div>
   </div>
 </template>
@@ -16,15 +16,17 @@
 <script>
 import axios from 'axios';
 import CardProp from '@/components/CardProp.vue'
+import Loader from '@/components/Loader.vue'
 export default {
     name: 'MainCards',
     components:{
         CardProp,
+        Loader,
     },
     data(){
         return{
             apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
-            MainCardsList: [],
+            MainCardsList: null,
         }
     },
     created(){
@@ -51,6 +53,7 @@ export default {
 @import '@/style/utilities';
     .my-container{
         width: 85%;
+        height: 100%;
         margin: 0 auto;
         padding-top: 70px;
     }
