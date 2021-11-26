0a4cde9c64446272dc1f4919cad9916a21920407
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-11-26 13:57:33 +0100

    add select

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,25 @@
 <template>
-  <header>
+  <header class="d-flex justify-content-between align-items-center">
       <img src="../assets/logo.png" alt="spotify-logo">
+      
+      <select v-model="genre" class="text-white">
+          <option value="">Genere Musicale</option>
+          <option value="Rock">Rock</option>
+          <option value="Pop">Pop</option>
+          <option value="Jazz">Jazz</option>
+          <option value="Metal">Metal</option>
+      </select>
   </header>
 </template>
 
 <script>
 export default {
     name: 'Header',
+    data(){
+        return{
+            genre: '',
+        }
+    }
 }
 </script>
 
@@ -18,6 +31,13 @@ export default {
         padding: 10px;
         img{
             height: 100%;
+        }
+
+        select{
+            width: 200px;
+            height: 50%;
+            background-color: $primary-color;
+            border-radius: 10px;
         }
     }
 </style>
